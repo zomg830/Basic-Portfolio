09c3d43c20edbcd5325f9fe7ab730ea25b1404f7
zomg830 <matt.faubel@gmail.com>
2019-04-01 09:36:49 -0500

    Updated portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown();
+    $('.carousel').carousel({
+        padding: 200,
+        dist: -50,
+    });
+    autoplay();
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 4500);
+    }
 });
